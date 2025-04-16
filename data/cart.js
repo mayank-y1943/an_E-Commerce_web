682b595b18371df842346c163e6de75e3eb96ae1
mayank-y1943 <mayankyada1943@gmail.com>
2025-04-16 16:56:15 +0530

    made code cleaner of generating html for cart

--- data/cart.js
+++ data/cart.js
@@ -1,23 +1,24 @@
 export const cart=[];
 
 export function addToCart(productId){
-    let selectedQuantity=document.querySelector(`.js-quantity-selector-${productId}`).value;
-  
-    let found=false;
-    cart.forEach((cartItem)=>{
-      if(cartItem.productId===productId){
-          cartItem.quantity+=Number(selectedQuantity);
-          found=true;
-      }
-    });
-  
-    if(!found){
-      cart.push(
-        {
-            productId: productId,
-            quantity: Number(selectedQuantity)
-        }
-      );
+  let selectedQuantity=document.querySelector(`.js-quantity-selector-${productId}`).value;
+
+  let found=false;
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId===productId){
+        cartItem.quantity+=Number(selectedQuantity);
+        found=true;
     }
-  
+  });
+
+  if(!found){
+    cart.push(
+      {
+          productId: productId,
+          quantity: Number(selectedQuantity)
+      }
+    );
   }
+}
+
+function 
