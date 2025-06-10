bbe820fc19e60279f9baf24785edaa5f2dd425e7
mayank-y1943 <mayankyada1943@gmail.com>
2025-06-10 15:03:22 +0530

    create class version of class

--- script/checkout.js
+++ script/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummeryHTML } from "./checkout/orderSummery.js";
 import { renderPaymentSummeryHTML } from "./checkout/paymentSummery.js";
-import '../data/cart-oop.js'
+import '../data/cart-class.js'
 
 renderOrderSummeryHTML();
 renderPaymentSummeryHTML();
