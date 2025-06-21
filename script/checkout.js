dd15c742c9b28a0a5f7d961446cf6d69766469e0
mayank-y1943 <mayankyada1943@gmail.com>
2025-06-21 15:23:43 +0530

    load product from backend

--- script/checkout.js
+++ script/checkout.js
@@ -1,7 +1,9 @@
 import { renderOrderSummeryHTML } from "./checkout/orderSummery.js";
 import { renderPaymentSummeryHTML } from "./checkout/paymentSummery.js";
+import { loadProduct } from "../data/products.js";
 // import '../data/cart-class.js;'
-import '../data/backend-practice.js';
-
-renderOrderSummeryHTML();
-renderPaymentSummeryHTML();
+// import '../data/backend-practice.js';
+loadProduct(()=>{
+    renderOrderSummeryHTML();
+    renderPaymentSummeryHTML();
+});
