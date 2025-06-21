265f170cd6e6afa550aeae06f1fe761dda8d8ba6
mayank-y1943 <mayankyada1943@gmail.com>
2025-06-21 10:52:15 +0530

    add backend file

--- script/checkout.js
+++ script/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummeryHTML } from "./checkout/orderSummery.js";
 import { renderPaymentSummeryHTML } from "./checkout/paymentSummery.js";
-// import '../data/cart-class.js'
+// import '../data/cart-class.js;'
+import '../data/backend-practice.js';
 
 renderOrderSummeryHTML();
 renderPaymentSummeryHTML();
