1fa4022800871cee2d1e0003f5c950499e880e67
mayank-y1943 <mayankyada1943@gmail.com>
2025-06-21 22:25:26 +0530

    use promise instead of callback to handle asynce awaits

--- script/checkout.js
+++ script/checkout.js
@@ -3,7 +3,20 @@ import { renderPaymentSummeryHTML } from "./checkout/paymentSummery.js";
 import { loadProduct } from "../data/products.js";
 // import '../data/cart-class.js;'
 // import '../data/backend-practice.js';
+
+new Promise((resolve)=>{
+    loadProduct(()=>{
+        resolve('value');
+    })
+}).then((value)=>{
+    console.log(value);
+    renderOrderSummeryHTML();
+    renderPaymentSummeryHTML();
+});
+
+/*
 loadProduct(()=>{
     renderOrderSummeryHTML();
     renderPaymentSummeryHTML();
 });
+*/
